b2feefb297a1cd3b0a1f9c80430cf11bca6a44c9
PeterAlfredLee <peteralfredlee@gmail.com>
2024-01-18 19:14:52 +0800

    update

--- src/App.vue
+++ src/App.vue
@@ -1,22 +1,22 @@
 <script setup lang='ts'>
 import Row_0 from './components/Row_0.vue'
 import Row_1 from './components/Row_1.vue'
-// import Row_2 from './components/Row_2.vue'
-// import Row_3 from './components/Row_3.vue'
-// import Row_4 from './components/Row_4.vue'
-// import Row_5 from './components/Row_5.vue'
-// import Row_6 from './components/Row_6.vue'
-// import Row_7 from './components/Row_7.vue'
-// import Row_8 from './components/Row_8.vue'
-// import Row_9 from './components/Row_9.vue'
-// import Row_10 from './components/Row_10.vue'
-// import Row_11 from './components/Row_11.vue'
-// import Row_12 from './components/Row_12.vue'
+import Row_2 from './components/Row_2.vue'
+import Row_3 from './components/Row_3.vue'
+import Row_4 from './components/Row_4.vue'
+import Row_5 from './components/Row_5.vue'
+import Row_6 from './components/Row_6.vue'
+import Row_7 from './components/Row_7.vue'
+import Row_8 from './components/Row_8.vue'
+import Row_9 from './components/Row_9.vue'
+import Row_10 from './components/Row_10.vue'
+import Row_11 from './components/Row_11.vue'
+import Row_12 from './components/Row_12.vue'
 import Row_13 from './components/Row_13.vue'
 import Row_14 from './components/Row_14.vue'
-// import Row_15 from './components/Row_15.vue'
-// import Row_16 from './components/Row_16.vue'
-// import Row_17 from './components/Row_17.vue'
+import Row_15 from './components/Row_15.vue'
+import Row_16 from './components/Row_16.vue'
+import Row_17 from './components/Row_17.vue'
 import Row_18 from './components/Row_18.vue'
 import Row_19 from './components/Row_19.vue'
 
@@ -26,22 +26,22 @@ import Row_19 from './components/Row_19.vue'
   <div class='root'>
     <Row_0 />
     <Row_1 />
-<!--          <Row_2 />-->
-<!--    <Row_3 />-->
-<!--    <Row_4 />-->
-<!--    <Row_5 />-->
-<!--    <Row_6 />-->
-<!--    <Row_7 />-->
-<!--    <Row_8 />-->
-<!--    <Row_9 />-->
-<!--    <Row_10 />-->
-<!--    <Row_11 />-->
-<!--    <Row_12 />-->
+    <Row_2 />
+    <Row_3 />
+    <Row_4 />
+    <Row_5 />
+    <Row_6 />
+    <Row_7 />
+    <Row_8 />
+    <Row_9 />
+    <Row_10 />
+    <Row_11 />
+    <Row_12 />
     <Row_13 />
     <Row_14 />
-<!--    <Row_15 />-->
-<!--    <Row_16 />-->
-<!--    <Row_17/>-->
+    <Row_15 />
+    <Row_16 />
+    <Row_17 />
     <Row_18 />
     <Row_19 />
   </div>
