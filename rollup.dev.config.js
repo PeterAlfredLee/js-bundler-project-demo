54fc4ce1348b9db86a67541df56deca6a89a24fa
PeterAlfredLee <peteralfredlee@gmail.com>
2024-01-23 10:12:34 +0800

    update rollup scripts

--- rollup.dev.config.js
+++ rollup.dev.config.js
@@ -1,6 +1,7 @@
 import vue from 'rollup-plugin-vue'
 import typescript from 'rollup-plugin-typescript2'
 import postcss from 'rollup-plugin-postcss'
+import serve from 'rollup-plugin-serve'
 
 export default {
   input: 'src/main.ts',
@@ -17,6 +18,11 @@ export default {
     postcss({ include: /(?<!&module=.*)\.css$/ }),
     typescript({
       check: false
+    }),
+    serve({
+      open: true,
+      contentBase: 'dist',
+      port: 8080
     })
   ]
 }
