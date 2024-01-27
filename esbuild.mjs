2b82d43cd19c0684731105796c00b455d6ed55c3
PeterAlfredLee <peteralfredlee@gmail.com>
2024-01-27 16:21:46 +0800

    update esbuild config

--- esbuild.mjs
+++ esbuild.mjs
@@ -1,73 +1,65 @@
-import esbuild from "esbuild";
-import vuePlugin from "esbuild-plugin-vue3"
-import alias from "esbuild-plugin-alias"
-import vue from "esbuild-vue"
-import http from "http";
-import path from "path";
-import url from "url";
-import fs from "fs";
+import esbuild from 'esbuild'
+import vuePlugin from 'esbuild-plugin-vue3'
+import http from 'http'
+import path from 'path'
+import url from 'url'
+import fs from 'fs'
 
 const dirname = path.dirname(url.fileURLToPath(import.meta.url))
-const args = process.argv.slice(2);
-const watch = args.includes("--watch");
-console.log(dirname)
+const args = process.argv.slice(2)
+const watch = args.includes('--watch')
 
 
 const context = await esbuild.context({
   plugins: [
-    vuePlugin(),
-    // alias({
-    //   '@': './src'
-    // })
+    vuePlugin({
+      generateHTML: watch ? 'esbuild-serve/index.html' : 'dist-esbuild/index.html'
+    })
   ],
-  entryPoints: [path.resolve(dirname, "src/main.ts")],
-  external: ['vue', '../*', './*'],
+  entryPoints: [path.resolve(dirname, 'src/main.ts')],
   bundle: true,
-  outfile: "esbuild-serve/main.js",
+  outfile: watch ? 'esbuild-serve/main.js' : 'dist-esbuild/main.js',
   sourcemap: true,
-  // banner: {
-  //   js: "(() => { (new EventSource(\"/esbuild\")).addEventListener('change', () => location.reload()); })();",
-  // },
   define: {
     __VUE_OPTIONS_API__: 'false',
     __VUE_PROD_DEVTOOLS__: 'false'
   }
-});
+})
 
-const serveDir = path.resolve(dirname, "esbuild-serve");
+const targetDir = watch ? path.resolve(dirname, 'esbuild-serve') : path.resolve(dirname, 'dist-esbuild')
 
-if (!fs.existsSync(serveDir)) {
-  fs.mkdirSync(serveDir)
+if (!fs.existsSync(targetDir)) {
+  fs.mkdirSync(targetDir)
 }
 
-fs.cpSync(path.resolve(dirname, "index.esbuild.html"), path.resolve(serveDir, "index.html"));
+fs.cpSync(path.resolve(dirname, 'index.esbuild.html'), path.resolve(targetDir, 'index.html'))
 
 if (watch) {
-  await context.watch();
-  let start = Date.now();
-  const {host, port} = await context.serve({
-    host: "localhost",
+  await context.watch()
+  let start = Date.now()
+  const { host, port } = await context.serve({
+    host: 'localhost',
     port: 1235,
-    servedir: "esbuild-serve",
-  });
-  let end = Date.now();
+    servedir: 'esbuild-serve'
+  })
+  let end = Date.now()
   const server = http.createServer((req, res) => {
-    const {method, headers} = req;
-    const url = new URL(req.url, `http://${req.headers.host}`);
-    const path = url.pathname === "/" ? "/index.html" : url.pathname;
+    const { method, headers } = req
+    const url = new URL(req.url, `http://${req.headers.host}`)
+    const path = url.pathname === '/' ? '/index.html' : url.pathname
     const proxyReq = http.request(
-      {hostname: host, port: port, path, method, headers},
+      { hostname: host, port: port, path, method, headers },
       function handler(proxyRes) {
-        res.writeHead(proxyRes.statusCode, proxyRes.headers);
-        proxyRes.pipe(res, {end: true});
+        res.writeHead(proxyRes.statusCode, proxyRes.headers)
+        proxyRes.pipe(res, { end: true })
       }
-    );
-    req.pipe(proxyReq, {end: true});
-  });
+    )
+    req.pipe(proxyReq, { end: true })
+  })
   server.listen(12345, () => {
-    console.log(`esbuild serve cost ${end - start}ms: http://localhost:12345/`);
-  });
+    console.log(`esbuild serve cost ${end - start}ms: http://localhost:12345/`)
+  })
 } else {
-  await context.rebuild();
-  context.dispose();
+  await context.rebuild()
+  context.dispose()
 }
